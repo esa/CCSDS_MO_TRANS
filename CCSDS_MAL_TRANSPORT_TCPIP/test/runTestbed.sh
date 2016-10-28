b80c104e33674280d747fb94881ba640bec03c81
Rian van Gijlswijk <r.vangijlswijk@telespazio-vega.de>
2016-10-28 14:43:07 +0000

    Updates to testbed runner script

--- CCSDS_MAL_TRANSPORT_TCPIP/test/runTestbed.sh
+++ CCSDS_MAL_TRANSPORT_TCPIP/test/runTestbed.sh
@@ -1,3 +1,11 @@
+if [ $# -eq 0 ]
+then
+	echo "No argument supplied for profile! You must provide a profile to run, i.e. ESA_TCPIP or SADT"
+	exit 1;
+fi
+
+echo "Running MAL Testbed using profile $1"
+
 cd CCSDS_MO_TRANS/
 mvn -P ESA clean install
 cd ../CCSDS_TCPIP_TESTBEDS/MOIMS_TESTBED_POM/
@@ -5,4 +13,4 @@ mvn clean install
 cd ../MOIMS_TESTBED_UTIL/
 mvn -P ESA clean install
 cd ../MOIMS_TESTBED_MAL/
-mvn -P ESA_TCPIP clean install
+mvn -P $1 clean install
